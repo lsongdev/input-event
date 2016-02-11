8a4d1651165e147643585cb6f7aca180659c3c73
Lsong <song940@gmail.com>
2016-02-11 13:39:23 +0800

    modify .

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
-const fs            = require('fs');
-const util          = require('util');
-const EventEmitter  = require('events');
+const fs     = require('fs');
+const util   = require('util');
+const events = require('events');
+/**
+ * [EVENT_TYPES]
+ * @type {Object}
+ * @docs https://www.kernel.org/doc/Documentation/input/event-codes.txt
+ */
+const EVENT_TYPES = {
+  EV_SYN      : 0x00,
+  EV_KEY      : 0x01,
+  EV_REL      : 0x02,
+  EV_ABS      : 0x03,
+  EV_MSC      : 0x04,
+  EV_SW       : 0x05,
+  EV_LED      : 0x06,
+  EV_SND      : 0x07,
+  EV_REP      : 0x08,
+  EV_FF       : 0x09,
+  EV_PWR      : 0x10,
+  EV_FF_STATUS: 0x11
+};
+
 /**
  * InputEvent
  */
 function InputEvent(device, options){
-  EventEmitter.call(this);
-  options = { flags: 'r', encoding: null };
+  events.EventEmitter.call(this);
   this.device  = device;
-  this.options = options;
-  this.fd = fs.createReadStream(device, options);
+  this.options = options || { flags: 'r', encoding: null };
+  this.fd = fs.createReadStream(this.device, this.options);
   this.fd.on('data', this.process.bind(this));
 }
+/**
+ * [inherits EventEmitter]
+ */
+util.inherits(InputEvent, events.EventEmitter);
 /**
  * [function parse]
  * @param  {[type]} buf [description]
@@ -26,13 +49,30 @@ InputEvent.prototype.parse = function(buf){
     value:   buf.readUInt32LE(12)
   };
 };
-
+/**
+ * [function description]
+ * @param  {[type]} data [description]
+ * @return {[type]}      [description]
+ */
 InputEvent.prototype.process = function(data){
   var ev = this.parse(data);
   this.emit('data', ev);
 };
 
-util.inherits(InputEvent, EventEmitter);
+/**
+ * [function close]
+ * @param  {Function} callback [description]
+ * @return {[type]}            [description]
+ */
+InputEvent.prototype.close = function(callback){
+  this.fd.close(callback);
+};
+/**
+ * [require description]
+ */
+InputEvent.EVENT_TYPES = EVENT_TYPES;
+InputEvent.Mouse    = require('./mouse');
+InputEvent.Keyboard = require('./keyboard');
 /**
  * [exports InputEvent]
  * @type {[type]}
--- test/test.js
+++ test/test.js
@@ -2,6 +2,20 @@ var InputEvent = require('../');
 
 var input = new InputEvent('/dev/input/event0');
 
-input.on('data', function(ev){
-  console.log(ev);
-});
+input.on('data', console.log);
+
+
+var keyboard = new InputEvent.Keyboard(input);
+
+keyboard.on('keyup'   , console.log);
+keyboard.on('keydown' , console.log);
+keyboard.on('keypress', console.log);
+
+
+var mouse = new InputEvent.Mouse(input);
+
+mouse.on('move'    , console.log);
+mouse.on('wheel'   , console.log);
+mouse.on('keyup'   , console.log);
+mouse.on('keydown' , console.log);
+mouse.on('keypress', console.log);
