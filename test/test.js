2490fb70edb7c46a694d2015b1eeca0d99f91e09
Lsong <song940@gmail.com>
2016-02-11 13:28:20 +0800

    use inherits .

--- test/test.js
+++ test/test.js
@@ -2,6 +2,21 @@ var InputEvent = require('../');
 
 var input = new InputEvent('/dev/input/event0');
 
-input.on('data', function(ev){
-  console.log(ev);
+var keyboard = new InputEvent.Keyboard(input);
+
+// keyboard.on('keyup'   , console.log);
+// keyboard.on('keydown' , console.log);
+keyboard.on('keypress', function(ev){
+  console.log(ev.code);
 });
+
+/*
+var mouse = new InputEvent.Mouse(input);
+
+mouse.on('move'    , console.log);
+mouse.on('wheel'   , console.log);
+mouse.on('keyup'   , console.log);
+mouse.on('keydown' , console.log);
+mouse.on('keypress', console.log);
+
+*/
