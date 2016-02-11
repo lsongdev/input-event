9a7e635013f748b0faecb9523e46757bdf092de4
Lsong <song940@gmail.com>
2016-02-11 13:28:20 +0800

    use inherits .

--- index.js
+++ index.js
@@ -1,45 +1,38 @@
 const fs            = require('fs');
+const util          = require('util');
 const EventEmitter  = require('events');
 /**
  * InputEvent
  */
-class InputEvent extends EventEmitter {
-  /**
-   * [constructor description]
-   * @param  {[type]} device  [description]
-   * @param  {[type]} options [description]
-   * @return {[type]}         [description]
-   */
-  constructor(device, options){
-    super();
-    options = { flags: 'r', encoding: null };
-    this.fd = fs.createReadStream(device, options);
-    this.fd.on('data', this.process.bind(this));
-  }
-  /**
-   * [parse description]
-   * @param  {[type]} buf [description]
-   * @return {[type]}     [description]
-   */
-  parse(buf){
-    return {
-      tssec:   buf.readUInt32LE(0),
-      tsusec:  buf.readUInt32LE(4),
-      type:    buf.readUInt16LE(8),
-      code:    buf.readUInt16LE(10),
-      value:   buf.readUInt32LE(12)
-    };
-  }
-  /**
-   * [process description]
-   * @param  {[type]} data [description]
-   * @return {[type]}      [description]
-   */
-  process(data){
-    var evt = this.parse(data);
-    this.emit('data', evt);
-  }
+function InputEvent(device, options){
+  EventEmitter.call(this);
+  options = { flags: 'r', encoding: null };
+  this.device  = device;
+  this.options = options;
+  this.fd = fs.createReadStream(device, options);
+  this.fd.on('data', this.process.bind(this));
 }
+/**
+ * [function parse]
+ * @param  {[type]} buf [description]
+ * @return {[type]}     [description]
+ */
+InputEvent.prototype.parse = function(buf){
+  return {
+    tssec:   buf.readUInt32LE(0),
+    tsusec:  buf.readUInt32LE(4),
+    type:    buf.readUInt16LE(8),
+    code:    buf.readUInt16LE(10),
+    value:   buf.readUInt32LE(12)
+  };
+};
+
+InputEvent.prototype.process = function(data){
+  var ev = this.parse(data);
+  this.emit('data', ev);
+};
+
+util.inherits(InputEvent, EventEmitter);
 /**
  * [exports InputEvent]
  * @type {[type]}
