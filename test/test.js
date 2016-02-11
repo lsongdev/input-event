01f41d137c49024934a14dc5e076cb1b851cc438
Lsong <song940@gmail.com>
2016-02-11 19:51:04 +0800

    fix #1, and add joystick game-pad control

--- test/test.js
+++ test/test.js
@@ -1,22 +1,24 @@
 var InputEvent = require('../');
 
-var input = new InputEvent('/dev/input/event0');
+var input = new InputEvent('/dev/input/js0');
 
-var keyboard = new InputEvent.Keyboard(input);
-
+// var keyboard = new InputEvent.Keyboard(input);
+// keyboard.on('data'    , console.log);
 // keyboard.on('keyup'   , console.log);
 // keyboard.on('keydown' , console.log);
-keyboard.on('keypress', function(ev){
-  console.log(ev.code);
-});
+// keyboard.on('keypress', console.log);
 
-/*
-var mouse = new InputEvent.Mouse(input);
+// var mouse = new InputEvent.Mouse(input);
+// mouse.on('data'    , console.log);
+// mouse.on('move'    , console.log);
+// mouse.on('wheel'   , console.log);
+// mouse.on('keyup'   , console.log);
+// mouse.on('keydown' , console.log);
+// mouse.on('keypress', console.log);
 
-mouse.on('move'    , console.log);
-mouse.on('wheel'   , console.log);
-mouse.on('keyup'   , console.log);
-mouse.on('keydown' , console.log);
-mouse.on('keypress', console.log);
-
-*/
+// var joystick = new InputEvent.JoyStick(input);
+// joystick.on('data'    , console.log);
+// joystick.on('move'    , console.log);
+// joystick.on('keyup'   , console.log);
+// joystick.on('keydown' , console.log);
+// joystick.on('keypress', console.log);
