4e2e72ec6cc0dbfd39b465771a901ea04d827abb
Lsong <song940@gmail.com>
2016-02-13 03:23:54 +0800

    add mice device support .

--- test/test.js
+++ test/test.js
@@ -1,6 +1,11 @@
 var InputEvent = require('../');
 
-var input = new InputEvent('/dev/input/js0');
+// var input = new InputEvent('/dev/input/event0');
+var input = new InputEvent('/dev/input/mice');
+//
+// input.on('data', function(buffer){
+//   console.log(buffer);
+// });
 
 // var keyboard = new InputEvent.Keyboard(input);
 // keyboard.on('data'    , console.log);
@@ -8,13 +13,14 @@ var input = new InputEvent('/dev/input/js0');
 // keyboard.on('keydown' , console.log);
 // keyboard.on('keypress', console.log);
 
-// var mouse = new InputEvent.Mouse(input);
+var mouse = new InputEvent.Mouse(input);
 // mouse.on('data'    , console.log);
 // mouse.on('move'    , console.log);
 // mouse.on('wheel'   , console.log);
 // mouse.on('keyup'   , console.log);
 // mouse.on('keydown' , console.log);
-// mouse.on('keypress', console.log);
+mouse.on('keypress', console.log);
+
 
 // var joystick = new InputEvent.JoyStick(input);
 // joystick.on('data'    , console.log);
