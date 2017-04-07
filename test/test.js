3fde2a91b52801189a280bccc35a23dbca077e71
basvanmeurs <basvanmeurs@live.com>
2017-04-07 10:27:50 +0200

    fixed this module based on observations in my system (64 bits, multiple events per line)

--- test/test.js
+++ test/test.js
@@ -1,25 +1,25 @@
 var InputEvent = require('../');
 
-// var input = new InputEvent('/dev/input/event0');
-var input = new InputEvent('/dev/input/mice');
+var input = new InputEvent('/dev/input/event6');
+//var input = new InputEvent('/dev/input/mice');
 //
 // input.on('data', function(buffer){
 //   console.log(buffer);
 // });
 
-// var keyboard = new InputEvent.Keyboard(input);
+var keyboard = new InputEvent.Keyboard(input);
 // keyboard.on('data'    , console.log);
-// keyboard.on('keyup'   , console.log);
-// keyboard.on('keydown' , console.log);
+keyboard.on('keyup'   , console.log);
+keyboard.on('keydown' , console.log);
 // keyboard.on('keypress', console.log);
 
-var mouse = new InputEvent.Mouse(input);
+//var mouse = new InputEvent.Mouse(input);
 // mouse.on('data'    , console.log);
 // mouse.on('move'    , console.log);
 // mouse.on('wheel'   , console.log);
 // mouse.on('keyup'   , console.log);
 // mouse.on('keydown' , console.log);
-mouse.on('keypress', console.log);
+//mouse.on('keypress', console.log);
 
 
 // var joystick = new InputEvent.JoyStick(input);
