0fcd1268312ce7c55ad5c9a4bcd2cd5014d4de08
Lsøng <song940@gmail.com>
2017-06-28 21:13:53 +0800

    Add files via upload

--- example/index.js
+++ example/index.js
@@ -1,19 +1,19 @@
-var InputEvent = require('../');
+const InputEvent = require('../');
 
-var input = new InputEvent('/dev/input/event6');
-//var input = new InputEvent('/dev/input/mice');
+const input = new InputEvent('/dev/input/event6');
+//const input = new InputEvent('/dev/input/mice');
 //
 // input.on('data', function(buffer){
 //   console.log(buffer);
 // });
 
-var keyboard = new InputEvent.Keyboard(input);
+const keyboard = new InputEvent.Keyboard(input);
 // keyboard.on('data'    , console.log);
 keyboard.on('keyup'   , console.log);
 keyboard.on('keydown' , console.log);
 // keyboard.on('keypress', console.log);
 
-//var mouse = new InputEvent.Mouse(input);
+//const mouse = new InputEvent.Mouse(input);
 // mouse.on('data'    , console.log);
 // mouse.on('move'    , console.log);
 // mouse.on('wheel'   , console.log);
@@ -21,8 +21,7 @@ keyboard.on('keydown' , console.log);
 // mouse.on('keydown' , console.log);
 //mouse.on('keypress', console.log);
 
-
-// var joystick = new InputEvent.JoyStick(input);
+// const joystick = new InputEvent.JoyStick(input);
 // joystick.on('data'    , console.log);
 // joystick.on('move'    , console.log);
 // joystick.on('keyup'   , console.log);
